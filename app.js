e6a5b1ca9e366876d441cd1b42a113b758baddaa
Ali Al Fattouhi Al Jundi <alfattoa@uwindsor.ca>
2026-03-18 01:01:36 +0000

    Add player ship and enemy to black background

---
+++ app.js
@@ -0,0 +1,37 @@
+async function loadTexture(path) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.src = path
+    img.onload = () => {
+      resolve(img)
+    }
+  })
+}
+
+function createEnemies(ctx, canvas, enemyImg) {
+      const MONSTER_TOTAL = 7;
+      const MONSTER_ROWS = 5;
+      const ENEMY_SIZE = 65;
+      const START_X = (canvas.width - (MONSTER_TOTAL * ENEMY_SIZE)) / 2;
+      for (let row = 0; row < MONSTER_ROWS; row++) {
+        for (let col = 0; col < MONSTER_TOTAL; col++) {
+          const x = START_X + col * ENEMY_SIZE;
+          const y = 60 + row * ENEMY_SIZE;
+          ctx.drawImage(enemyImg, x, y, ENEMY_SIZE, ENEMY_SIZE);
+        }
+      }
+    }
+
+window.onload = async () => {
+  canvas = document.getElementById('myCanvas')
+  ctx = canvas.getContext('2d')
+  // TODO load textures 
+  const heroImg = await loadTexture("../assets/player.png")
+  const enemyImg = await loadTexture("../assets/enemyShip.png")
+  // TODO draw black background
+  ctx.fillStyle="black";
+  ctx.fillRect(0,0);
+  // TODO draw hero
+  ctx.drawImage(heroImg, canvas.width/2 - 45, canvas.height - (canvas.height / 4), 90, 90);
+  createEnemies(ctx, canvas, enemyImg);
+}
